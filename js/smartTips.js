68a120540d06a7272c06974e2e2ce0b8ff0f70ef
Omar Rahman <rahman.omar@gmail.com>
2015-03-02 00:24:02 -0500

    finished alpha rel. Style complete, defintiions in. Extraneous theme elements deleted

--- js/smartTips.js
+++ js/smartTips.js
@@ -7,7 +7,7 @@ $(function() {
 // Our list of terms and their corresponding definitions
 var definitions = {
 	tooltips : "Messages that appear when a cursor is positioned over a term, icon, image, hyperlink, or other element in a graphical user interface.",
-	smarttips: "smartTips is not a registered trademark.",
+	smarttips: "The smart and easy tooltip generator",
   'web project': "You just have to include the smartTips.js script",
   plugin : "In computing, a plug-in (or add-in / addin, plugin, extension or add-on / addon) is a software component that adds a specific feature to an existing application. When an application supports plug-ins, it enables customization.",
   ' is that' : "Answer: pretty effing cool!",
@@ -15,6 +15,9 @@ var definitions = {
   'romanmahar' : "Professional alias and anagram for OmarRahman, creator of this plugin",
   webpage : "A web page (or webpage) is a web document that is suitable for the World Wide Web and the web browser.",
   'class of .smarttip' : "The default class that is applied to the span that surrounds the word. This lets you style the smartTip however you want",
+  '.smarttip ' : "The span class surrounding each of the definition words",
+  '.tooltip ' : "the class of the tooltip popup",
+  ' tooltip ' : "One of these"
 }
 
 // This function will find the strings in "words :" and surround them with a span tag
@@ -32,9 +35,9 @@ $.fn.wrapInTag = function(opts) {
 };
 
 // for this demo file I have chosen to select strings 
-// within <h1 /> elements as well a <p /> elements
-// You can choose to look for strings in just <p /> or 
-// anything that you would like to search in really elements
+// within 'h1', 'p', 'ol li', and 'a' elements
+// You can choose to look for strings in just 'p' or 
+// anything that you would like to search in
 $('p, h1, ol li, a').wrapInTag({
   tag: 'span class="smartTip"',
   // applies Span tag to strings that match definitions.
